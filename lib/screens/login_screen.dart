b974c3a9346b691bac3f0767942a3c59e2209451
souza.leandror@gmail.com <souza.leandror@gmail.com>
2019-07-22 20:05:47 -0300

    Update 20%

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:gerente_loja/blocs/login_bloc.dart';
+import 'package:gerente_loja/widgets/input_field.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _loginBloc = LoginBloc();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.grey[850],
+        body: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(),
+            SingleChildScrollView(
+              child: Container(
+                margin: EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Icon(
+                      Icons.store_mall_directory,
+                      color: Colors.pinkAccent,
+                      size: 160,
+                    ),
+                    InputField(
+                      icon: Icons.person_outline,
+                      hint: "Usuario",
+                      obscure: false,
+                      stream: _loginBloc.outEmail,
+                      onChanged: _loginBloc.changeEmail,
+                    ),
+                    InputField(
+                      icon: Icons.lock_outline,
+                      hint: "Senha",
+                      obscure: true,
+                      stream: _loginBloc.outPassword,
+                      onChanged: _loginBloc.changePassword,
+                    ),
+                    SizedBox(
+                      height: 32,
+                    ),
+                    StreamBuilder<bool>(
+                        stream: _loginBloc.outSubmitValid,
+                        builder: (context, snapshot) {
+                          return SizedBox(
+                              height: 50,
+                              child: RaisedButton(
+                                color: Colors.pinkAccent,
+                                child: Text("Entrar"),
+                                onPressed:
+                                    snapshot.hasData ? _loginBloc.submit : null,
+                                textColor: Colors.white,
+                                disabledColor: Colors.pinkAccent.withAlpha(140),
+                              ));
+                        })
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
---
+++ lib/validators/login_validators.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+class LoginValidators {
+  final validateEmail =
+      StreamTransformer<String, String>.fromHandlers(handleData: (email, sink) {
+    if (email.contains("@")) {
+      sink.add(email);
+    } else {
+      sink.addError("Insira um e-mail valido !");
+    }
+  });
+
+  final validatePassword = StreamTransformer<String, String>.fromHandlers(
+      handleData: (password, sink) {
+    if (password.length > 4) {
+      sink.add(password);
+    } else {
+      sink.addError("Senha invalida, deve conter pelos 4 caracteres !");
+    }
+  });
+}
